ee85ca49fa303b6162f2b30d0e22af9fe2621ce5
tasyahira <81616835+tasyahira@users.noreply.github.com>
2024-03-15 00:01:50 +0700

    add script

---
+++ ddl-source.sql
@@ -0,0 +1,23 @@
+CREATE TABLE IF NOT EXISTS covid_data (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    CLOSECONTACT INT,
+    CONFIRMATION INT,
+    PROBABLE INT,
+    SUSPECT INT,
+    closecontact_dikarantina INT,
+    closecontact_discarded INT,
+    closecontact_meninggal INT,
+    confirmation_meninggal INT,
+    confirmation_sembuh INT,
+    kode_kab VARCHAR(255),
+    kode_prov VARCHAR(255),
+    nama_kab VARCHAR(255),
+    nama_prov VARCHAR(255),
+    probable_diisolasi INT,
+    probable_discarded INT,
+    probable_meninggal INT,
+    suspect_diisolasi INT,
+    suspect_discarded INT,
+    suspect_meninggal INT,
+    tanggal DATE
+);
